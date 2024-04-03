98e52b1bc402689e663dabe9dc4ffc0a3c01314d
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-04-03 16:24:21 -0300

    Component Library - Utilizando Variantes.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,23 @@ type IProps = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, ...rest }: IProps) {
+export function Button({ title, variant, ...rest }: IProps) {
   return (
     <ButtonNativeBase
       h={14}
       width={"full"}
       rounded={"sm"}
-      bg={"green.700"}
-      _pressed={{ bg: "green.500" }}
+      borderColor={"green.500"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      _pressed={{ bg: variant === "outline" ? "gray.500" : "green.500" }}
       {...rest}
     >
-      <Text color={"white"} fontFamily={"heading"} fontSize={"sm"}>
+      <Text
+        fontSize={"sm"}
+        fontFamily={"heading"}
+        color={variant === "outline" ? "green.500" : "white"}
+      >
         {title}
       </Text>
     </ButtonNativeBase>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -38,6 +38,8 @@ export function SignIn() {
 
         <Button title="Acessar" />
       </Center>
+
+      <Button title="Criar conta" variant={"outline"} />
     </VStack>
   );
 }
