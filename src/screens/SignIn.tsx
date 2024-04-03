18bf557654a16b8ce2a4203d8be72a272c9699b7
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-04-03 15:41:53 -0300

    Component Library - Utilizando SVG.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
-import { Image, VStack } from "native-base";
+import { Center, Image, Text, VStack } from "native-base";
 
+import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
 export function SignIn() {
@@ -11,6 +12,14 @@ export function SignIn() {
         source={BackgroundImg}
         alt="Pessoas treinando"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   );
 }
