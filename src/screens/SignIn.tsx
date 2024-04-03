c98f2d6891e13a6d7bce218655d3343bc8931897
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-04-03 15:45:27 -0300

    Component Library - Componente Heading.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -19,6 +19,17 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading
+          mb={6}
+          fontSize={"xl"}
+          color={"gray.100"}
+          fontFamily={"heading"}
+        >
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
